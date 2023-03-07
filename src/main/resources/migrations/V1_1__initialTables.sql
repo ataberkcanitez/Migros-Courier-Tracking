5e8304d5903ebe4c5b4a5708abc30fa3e0065e2b
Ataberk Canıtez <aticanitez@hotmail.com>
2023-03-07 17:14:12 +0300

    added insert scripts into 'store' table

--- src/main/resources/migrations/V1_1__initialTables.sql
+++ src/main/resources/migrations/V1_1__initialTables.sql
@@ -26,3 +26,11 @@ CREATE TABLE entrances (
   FOREIGN KEY (storeId) REFERENCES stores(id),
   FOREIGN KEY (courierId) REFERENCES courier(id)
 );
+
+INSERT INTO stores (name, latitude, longitude)
+VALUES
+  ('Ataşehir MMM Migros', 40.9923307, 29.1244229),
+  ('Novada MMM Migros', 40.986106, 29.1161293),
+  ('Beylikdüzü 5M Migros', 41.0066851, 28.6552262),
+  ('Ortaköy MMM Migros', 41.055783, 29.0210292),
+  ('Caddebostan MMM Migros', 40.9632463, 29.0630908);
