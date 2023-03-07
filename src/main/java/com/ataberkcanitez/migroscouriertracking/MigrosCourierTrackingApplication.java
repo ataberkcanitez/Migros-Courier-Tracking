f208623c648842210c7051d97e983c7c44a1556b
Ataberk Canıtez <aticanitez@hotmail.com>
2023-03-07 15:02:14 +0300

    initial files

---
+++ src/main/java/com/ataberkcanitez/migroscouriertracking/MigrosCourierTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.ataberkcanitez.migroscouriertracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrosCourierTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrosCourierTrackingApplication.class, args);
+	}
+
+}
